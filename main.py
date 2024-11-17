80c3ddee9e422e7e671a7b9b05ff637fc8ce5109
ksh <argoooooon@gmail.com>
2024-11-17 14:32:06 +0900

    Add logger

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from os import environ
+import logging
 
 from elasticsearch import Elasticsearch
 import ranx
 
-from src.dataset import get_dataset
-from src.model import get_search_model
+from splade_es.dataset import get_dataset
+from splade_es.model import get_search_model
 
+
+logger = logging.getLogger("splade_es")
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s-[%(name)s][%(levelname)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
 
 
 def main():
     es_client = Elasticsearch(environ["ELASTICSEARCH_URL"])
 
-    dataset_name = "nfcorpus"
+    dataset_name = "scifact"
     dataset = get_dataset(dataset_name)
 
     # search_model = get_search_model("bm25")(es_client, dataset, reset_index=True)
     search_model = get_search_model("splade")(es_client, dataset, reset_index=True)
 
-    # search_model.index(dataset.corpus_iter())
+    DEBUG = False
+    corpus = dataset.corpus_iter()
+    if DEBUG:
+        corpus = [doc for i, doc in enumerate(dataset.corpus_iter()) if i < 3]
+
+    search_model.index(corpus)
     search_result = search_model.search(dataset.queries)
 
     # Print 10 search result examples:
@@ -37,6 +50,5 @@ def main():
     print(eval_result)
 
 if __name__ == "__main__":
-    from src.model.bm25 import logger
-    logger.setLevel("DEBUG")
+    logger.setLevel(logging.DEBUG)
     main()
